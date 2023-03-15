374d32bc982c0884c8b5b64490019d866c5e305a
Rodrigo Anchieta <rodrigoganchieta@gmail.com>
2023-03-15 12:22:23 -0300

    module control

--- menu.php
+++ menu.php
@@ -35,4 +35,17 @@
       <li><a href="exercicio.php?dir=variaveis&file=constantes">Constantes</a></li>
     </ul>
   </div>
+  <div class="modulo roxo">
+    <h3>4. Controle</h3>
+    <ul>
+      <li><a href="exercicio.php?dir=controle&file=if_else">If Else</a></li>
+        <li><a href="exercicio.php?dir=controle&file=operadores_relacionais">Op. Relacionais</a></li>
+        <li><a href="exercicio.php?dir=controle&file=desafio_pi">Desafio PI</a></li>
+        <li><a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores Lógicos</a></li>
+        <li><a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">Desafio Op. Lógicos</a></li>
+        <li><a href="exercicio.php?dir=controle&file=operador_ternario">Operador Ternário</a></li>
+        <li><a href="exercicio.php?dir=controle&file=switch">Switch</a></li>
+        <li><a href="exercicio.php?dir=controle&file=desafio_switch">Desafio Switch</a></li>
+    </ul>
+  </div>
 </nav>
