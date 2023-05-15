4ffe5ce7a347d476df7d52934a69370558c5ae21
Rodrigo Anchieta <rodrigoganchieta@gmail.com>
2023-05-15 12:45:33 -0300

    Namespace

--- menu.php
+++ menu.php
@@ -148,4 +148,24 @@
           </li>
       </ul>
   </div>
+  <div class="modulo laranja-escuro">
+      <h3>10. Namespace</h3>
+      <ul>
+          <li>
+              <a href="exercicio.php?dir=namespace&file=basico">
+                  Exemplo Básico
+              </a>
+          </li>
+          <li>
+              <a href="exercicio.php?dir=namespace&file=sub_namespaces">
+                  Sub-Namespaces
+              </a>
+          </li>
+          <li>
+              <a href="exercicio.php?dir=namespace&file=use_as">
+                  Use/As
+              </a>
+          </li>
+      </ul>
+  </div>
 </nav>
