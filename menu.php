ae2761d112f81aa0c77b63bcc5c826f4802a2bd3
Rodrigo Anchieta <rodrigoganchieta@gmail.com>
2023-05-25 18:02:04 -0300

    Form

--- menu.php
+++ menu.php
@@ -248,4 +248,14 @@
       </li>
     </ul>
   </div>
+  <div class="modulo roxo">
+    <h3>14. Formulário</h3>
+    <ul>
+      <li>
+        <a href="exercicio.php?dir=formulario&file=formulario">
+          Formulário
+        </a>
+      </li>
+    </ul>
+  </div>
 </nav>
